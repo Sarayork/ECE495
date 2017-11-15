247422590055932db894becb53c87b6bdea524c5
Sarayork <sarayork@umail.iu.edu>
2017-11-15 01:00:23 -0500

    Add files via upload
    added rest of the things I've implemented in past assignments, still adding things

--- sara_folder/HARNESS.go
+++ sara_folder/HARNESS.go
@@ -14,20 +14,13 @@ import(
 
 
 type Patient struct{
-
-  ID string `json: "id,omitempty"`
-  Name string `json: "patname,omitempty"`
-  Telecom string  `json: "telecom"`
-  Gender string `json: "gender"`
-  BirthDate string `json:"birthDate"`
-
-	Family string `json:"family, omitempty"`
-	Given string `json:"given"`
-	Prefix string `json:"prefix"`
-
-	System string `json:"system"`
-	Value string `json:"value"`
-	Use string `json:"use"`
+  ID string `json:"id,omitempty"`
+  Firstname string `json:"firstname,omitempty"`
+  Lastname string `json:"lastname,omitempty"`
+  Medication string `json:"medication,omitempty"`
+  Encounter string `json: "encounter,omitempty"`
+  Condition string `json: "condition, omitempty"`
+  Contact string `json: "contact,omitempty"`
 }
 //create array of patients that are type patient from the struct
 var Patients []Patient
@@ -88,8 +81,7 @@ func GetPatientByID(w http.ResponseWriter, r *http.Request){
   //var ID string
   var person Patient
 
-  _ = json.NewDecoder(r.Body).Decode(&person)
-
+//load the html and ask for ID
   fmt.Println("method: ", r.Method)
   if r.Method == "GET"{
     t, _ := template.ParseFiles("askForID.html")
@@ -99,7 +91,11 @@ func GetPatientByID(w http.ResponseWriter, r *http.Request){
     fmt.Println("ID: ", r.Form["ID"])
     person.ID = r.FormValue("ID")
   }
+  //encode the person and save them into the local file
+  json.NewEncoder(w).Encode(person)
+  UpdateRecord(person)
 
+//tried to retrieve FHIR data for the patients ID
   url := fmt.Sprintf("http://fhirtest.uhn.ca/baseDstu3/Patient" + person.ID)
   r, err := http.NewRequest("GET", url, nil)
   r.Header.Set("Content-Type", "application/json")
@@ -109,7 +105,6 @@ func GetPatientByID(w http.ResponseWriter, r *http.Request){
   resp := client.Do(r)
   defer resp.Body.Close()
 
-  json.NewEncoder(w).Encode(Patients)
 
 }//end of look up by ID
 /////////////////////////////////////////////////////////////////////////
@@ -128,6 +123,7 @@ func GetPatientByName(w http.ResponseWriter, r *http.Request){
     fmt.Println("Patient: ", r.Form["Patient"])
     person.Name = r.FormValue("Patient")
   }
+  //
 
   url := fmt.Sprintf("http://fhirtest.uhn.ca/baseDstu3/Patient" + person.Name)
   r, err := http.NewRequest("GET", url, nil)
@@ -143,9 +139,10 @@ func GetPatientByName(w http.ResponseWriter, r *http.Request){
 ////////////////////////////////////////////////////////////////////////////
 func GetMedication(w http.ResponseWriter, r *http.Request){
 
+
   var person Patient
   var medicationID string
-
+//loads the html file to display to the web browser
   fmt.Println("method: ", r.Method)
   if r.Method == "GET"{
     t, _ := template.ParseFiles("Medication.html")
@@ -155,7 +152,8 @@ func GetMedication(w http.ResponseWriter, r *http.Request){
     fmt.Println("Medication: ", r.Form["Medication"])
     medicationID = r.FormValue("Medication")
   }
-
+//
+//tried to retrieve FHIR information with user input
   url := fmt.Sprintf("http://fhirtest.uhn.ca/baseDstu3/Medication" + medicationID)
   r, err := http.NewRequest("GET", url, nil)
   r.Header.Set("Content-Type", "application/json")
@@ -165,7 +163,78 @@ func GetMedication(w http.ResponseWriter, r *http.Request){
   resp := client.Do(r)
   defer resp.Body.Close()
 
+}
+//////////////////////////////////////////////////
+func LoadData(){
+  file := ioutil.ReadFile("PatientInfo.json")
+  json.Unmarshall([]byte(string(file)), &Patiens)
+}
+//////////////////////////////////////////////////////
+func SaveData(Data []Patient){
+  st := json.Marshal(Data)
 
+  fo := os.Create("PatientInfo.json")
+  defer func(){
+    if err := fo.Close(); err != nil{
+      panic(err)
+    }
+  }()
+  fo.Write(str)
+}
+//////////////////////////////////////////////////////////
+func UpdateRecord(Data Patient){
+  Patients = append(Patients, Data)
+  SaveData(Patients)
+}
+////////////////////////////////////////////////////////////
+func GetCondition(w http.ResponseWriter, r *http.Request){
+  var person Patient
+  var condition string
+//loads the html file to display to the web browser
+  fmt.Println("method: ", r.Method)
+  if r.Method == "GET"{
+    t, _ := template.ParseFiles("Condition.html")
+    t.Execute(w, nil)
+  } else {
+    r.ParseForm()
+    fmt.Println("Condition: ", r.Form["Condition"])
+    condition = r.FormValue("Condition")
+  }
+}
+///////////////////////////////////////////////////////
+func GetEncounter(w http.ResponseWriter, r *http.Request){
+  var person Patient
+  var encounter string
+//loads the html file to display to the web browser
+  fmt.Println("method: ", r.Method)
+  if r.Method == "GET"{
+    t, _ := template.ParseFiles("Encounter.html")
+    t.Execute(w, nil)
+  } else {
+    r.ParseForm()
+    fmt.Println("Encounter: ", r.Form["Encounter"])
+    encounter = r.FormValue("Encounter")
+  }
+}
+//////////////////////////////////////////////////////////////
+func AddContact(w http.ResponseWriter, r *http.Request){
+  var person Patient
+  var person Patient
+  var contact string
+//loads the html file to display to the web browser
+  fmt.Println("method: ", r.Method)
+  if r.Method == "GET"{
+    t, _ := template.ParseFiles("AddContact.html")
+    t.Execute(w, nil)
+  } else {
+    r.ParseForm()
+    fmt.Println("Contact: ", r.Form["Contact"])
+    contact = r.FormValue("Contact")
+  }
+
+  person.Contact = contact
+  json.NewEncoder(w).Encode(person)
+  UpdateRecord(person)
 }
 func main(){
 //all the http handles are going to be in this already
@@ -173,12 +242,17 @@ func main(){
 //this  server handles the HARNESS part and the data transfer
 //for the clients
 router := mux.NewRouter()
+LoadData()
 
 router.HandleFunc("/Patient", GetPatients).Methods("GET")
 router.HandleFunc("/log", UserLogin).Methods("GET")
 router.HandleFunc("/lkuID", GetPatientByID).Methods("GET")
 router.HandleFunc("/lku", GetPatientByName).Methods("GET")
 router.HandleFunc("/med", GetMedication).Methods("GET")
+router.HandleFunc("/cond", GetCondition).Methods("GET")
+router.HandleFunc("/enc", GetEncounter).Methods("GET")
+router.HandleFunc("/rmc", RemoveContact).Methods("DELETE")
+router.HangleFunc("/add", AddContact).Methods("POST")
 
 http.ListenAndServe(":9090", router)
 
